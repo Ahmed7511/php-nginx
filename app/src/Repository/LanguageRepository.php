f14d2abcf00d353e28d844bf1288c578bbff0208
ahmed <sidouparadox7@gmail.com>
2022-12-26 11:15:01 +0100

    add-redis

--- app/src/Repository/LanguageRepository.php
+++ app/src/Repository/LanguageRepository.php
@@ -3,13 +3,12 @@
 namespace App\Repository;
 
 use App\Model\Language;
-
 class LanguageRepository extends Repository
 {
     private string $table = Language::TABLE;
 
     public function findAll(): array
     {
-        return $this->connection->query("SELECT * FROM {$this->table}")->fetchAll(\PDO::FETCH_CLASS, Language::class);
+       return $this->connection->query("SELECT * FROM {$this->table}")->fetchAll(\PDO::FETCH_CLASS, Language::class);
     }
 }
--- app/src/Repository/TranslationRepository.php
+++ app/src/Repository/TranslationRepository.php
@@ -7,7 +7,7 @@ class TranslationRepository extends Repository
     public function findForLanguage($languageId, $phrase): ?string
     {
         $stmt = $this->connection->prepare("SELECT translation FROM translation WHERE language_id = :language AND phrase = :phrase");
-
+      
         $stmt->execute(['language' => $languageId, 'phrase' => $phrase]);
 
         return $stmt->fetchColumn();
